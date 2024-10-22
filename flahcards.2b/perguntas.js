45a7f061d8ece1f6f532077cdc0c391a30f62c44
Maria <140732755+marialuisaandrade@users.noreply.github.com>
2024-10-22 11:58:30 -0300

    Update perguntas.js

--- flahcards.2b/perguntas.js
+++ flahcards.2b/perguntas.js
@@ -1,1 +1,11 @@
+criaCartao(
+    'Biologia',
+    'O que são vacinas? ',
+    'Produtos biológicos que estimulam a defesa do corpo contra alguns microorganismo que provocam doenças'
+)
 
+criaCartao(
+    'Empreendedorismo',
+  'Qual o conceito de escabilidade',
+    'Capacidade que uma empresa tem de sustentar ou de melhorar seu desempenho'
+)
